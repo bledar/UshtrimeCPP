89013497a2e4dbe1865d73f1bceb9842cdb01521
Bledar <bledi-1@live.com>
2016-09-02 15:49:22 +0200

    llogarit shume  ene numrave

---
+++ Ushtimi_7_Seria.cpp
@@ -0,0 +1,39 @@
+//============================================================================
+// Name        : Ushtrimi_7.cpp
+// Author      : Bledar Haxhia
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+
+// Te ndertohet nje funksion qe gjene vleren e funksionit
+// y= X* ∑(2*i/3-4) (i=2 deri ne n+1)
+// vlera x jepet nga perdorusi
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int n =5;
+
+float llogarit_serine(int x){
+
+	float y=0;
+
+	for(int i=2; i<=(n+1);i++){
+		y=y+((2*i)/3-4);
+	}
+
+	y=y*x;
+
+	return y;
+}
+
+int main() {
+	int x=0;
+	cout << "Jepni vleren X:" << endl;
+	cin>>x;
+
+	cout<<"Shuma: "<<llogarit_serine(x);
+
+	return 0;
+}
